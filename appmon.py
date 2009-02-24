fec9ed2272cd9a52f2ab7b95a7983ed926a9342f
Martin Renold <martinxyz@gmx.ch>
2009-02-24 21:02:55 +0100

    remove duplicates of application names

--- appmon.py
+++ appmon.py
@@ -7,8 +7,8 @@ import select
 xrecord_child = subprocess.Popen('x11/activity_monitor', stdout=subprocess.PIPE)
 focus_child = subprocess.Popen('x11/getfocuswindow', stdout=subprocess.PIPE)
 
-slotduration = 5*60.0 # seconds
-#slotduration = 3
+#slotduration = 5*60.0 # seconds
+slotduration = 3
 lastslot = time.time()
 
 slot_applications = []
@@ -27,7 +27,7 @@ while True:
     t = time.time()
     while t > lastslot + slotduration:
         # finish the last slot first
-        print int(time.time()), int(slot_activity), slot_applications
+        print int(time.time()), int(slot_activity), list(set(slot_applications))
         sys.stdout.flush()
         if slot_applications:
             slot_applications = [slot_applications[-1]]
